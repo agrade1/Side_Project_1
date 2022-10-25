2dc7e8e2eb4161fc20500507a81105599c1032b9
agrade1 <98932636+agrade1@users.noreply.github.com>
2022-10-25 17:28:53 +0900

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ window.onload = function () {
     //볓번째 리스트인지 체크할 변수
     let showList = 0;
 
-    for (i = 0; i < menuList.length; i++) {
+    for (i = 0; i<menuList.length; i++) {
         menuList[i].classList.add("on");
     }
     menuBtn.forEach((n, id) => {
@@ -18,11 +18,11 @@ window.onload = function () {
     function clickBtn(e) {
         e.preventDefault();
         if (showList == 0){
-            for (i = 0; i < menuList.length; i++) {
+            for (i = 0; i<menuList.length; i++) {
                 menuList[i].classList.add("on");
             }
         } else {
-            for (i = 0; i < menuList.length; i++) {
+            for (i = 0; i<menuList.length; i++) {
                 menuList[i].classList.remove("on");
             }
             menuList[showList-1].classList.add("on");
